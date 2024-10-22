b0f435aa7ce7d501713fa110c066e9e279593f08
Anjola Adeyemi <anjolaadeyemi4545@gmail.com>
2024-10-22 05:15:56 +0100

    nft's

---
+++ src/BasicNft.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+
+pragma solidity ^0.8.24;
+
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract BasicNft {
+    constructor() {
+        
+    }
+}
