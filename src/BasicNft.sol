6909d9abeadeaede917d6cc2da8ecd295863c9c0
Anjola Adeyemi <anjolaadeyemi4545@gmail.com>
2024-10-23 17:55:30 +0100

    Mean it

--- src/BasicNft.sol
+++ src/BasicNft.sol
@@ -6,8 +6,19 @@ pragma solidity ^0.8.24;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract BasicNft is ERC721 {
-    uint 23
+    uint256 private s_tokenCounter
     constructor() ERC721("Bougie", "BNFT") {
-        
+        s_tokenCounter = 0;
     }
+
+    function mintNFT()  public {}
+
+    function tokenURI( 
+        uint256 tokenID
+
+    ) public view override returns (string memory)
+    {
+  return
+
+     }
 }
