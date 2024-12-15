48a4224b266bdb8bb0e5b044dc2a29cb466ab5ad
Anjola Adeyemi <anjolaadeyemi4545@gmail.com>
2024-12-15 22:47:06 +0100

    base64

--- src/MoodNft.sol
+++ src/MoodNft.sol
@@ -2,22 +2,24 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.24;    // Version of solidity
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";    // Importing ERC721 contract from openzeppelin
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";    // Importing ERC721 contract from openzeppelin
+import {Base64} from "@openzepplin/contracts/utils/Base64.sol"
+
 
 contract MoodNft is ERC721{
-    uint256 s_tokenCounter;    // Counter to keep track of the number of NFTs minted
-    string private s_sadSvg;    // SVG for sad mood
-    string private s_happySvg;    // SVG for happy mood 
+    uint256 private s_tokenCounter;    // Counter to keep track of the number of NFTs minted
+    string private s_sadSvgImageUri;    // SVG for sad mood
+    string private s_happySvgImageUri;    // SVG for happy mood 
 
 
 
     constructor(
-    string memory sadSvgImageURi,    // Constructor to initialize the SVGs
-    string memory happySvgImageURi
+    string memory sadSvgImageUri,    // Constructor to initialize the SVGs
+    string memory happySvgImageUri
     ) public ERC721("MoodNft", "MNFT") {    // Constructor to initialize the name and symbol of the NFT
         s_tokenCounter = 0;
-        s_sadSvg = sadSvgUri;
-        s_happySvg = happySvgUri;   // Initialize the SVGs
+        s_sadSvgImageUri = sadSvgUri;
+        s_happySvgImageUri = happySvgUri;   // Initialize the SVGs
 
 
     }
@@ -30,6 +32,8 @@ contract MoodNft is ERC721{
 
     }
     function tokenURI(uint256 tokenID) public view override returns (string memory){
+        string memory tokenMetaData = string.concat('{"name": name(), ''"}');
+        
         if(tokenID % 2 == 0){
             return s_sadSvg;    // Return sad SVG if tokenID is even
         } else {
